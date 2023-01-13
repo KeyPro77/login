d61684146ad0321ac6d52b24817de718a024b618
KeyPro77 <93216062+KeyPro77@users.noreply.github.com>
2023-01-13 09:48:12 +0100

    Update login.js

--- login.js
+++ login.js
@@ -17,9 +17,5 @@ loginButton.addEventListener("click", (e) => {
     } else {
         loginErrorMsg.style.opacity = 1;
     }
-    if (username === "abdullahkabalan09@gmail.com" && password === "160380AbD") {
-        location.href = "https://keypro77.github.io/htmlctm";
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
+
 })
